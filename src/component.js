c09483c59a118b32f79500058a96c4a07553bce0
Linghua Jin <ljin@walmartlabs.com>
2015-07-09 02:52:44 -0700

    initial commit

--- src/component.js
+++ src/component.js
@@ -2,10 +2,25 @@
 
 import React from 'react/addons';
 
+
+
 const Component = React.createClass({
   render() {
-    return <div>The component Component should render here.</div>;
-  }
-});
+    return (
+      <div className="card">
+        <div className="image">
+          <img src="http://i.imgur.com/WerlaFe.jpg"/>
+            <span className="title">Card Title</span>
+          </div>
+          <div className="content">
+            <p>100 push ups</p>
+          </div>
+          <div className="progress">
+            <div className="determinate"></div>
+          </div>
+        </div>
+      );
+    }
+  });
 
-export default Component;
+  export default Component;
